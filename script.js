b0a3695ea5437a180160340338d553f532478fe0
“kaya <kaya.luanah@gmail.com>
2019-06-12 14:35:47 -0300

    construir escada

--- script.js
+++ script.js
@@ -19,11 +19,22 @@
 // -----------------------------------------------------------------------------------
 
 
-let nome2 = prompt("Digite seu nome:")
+// let nome2 = prompt("Digite seu nome:")
 
-alert(`seu nome é ${nome2}`)
+// alert(`seu nome é ${nome2}`)
 
 
+// ------------------------------construir escada------------------------------------------
+
+
+let degraus = prompt("insira o numero de degraus desejado: ")
+let simbolo = prompt("escolha o simbolo desejado entre #, * e $")
+let j = simbolo
+
+for(let i=0;i<degraus;i++){
+        console.log(simbolo)
+        simbolo += j
+}
 
 
  
