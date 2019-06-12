a42c9d6b590f52f96b5824bd1d78edae7db548d8
“kaya <kaya.luanah@gmail.com>
2019-06-12 16:23:09 -0300

    utilizar funçao para elaborar uma calculadora

--- script.js
+++ script.js
@@ -39,21 +39,61 @@
 
 // -----------------filmes-------------------------------------------------------------
 
-let filmes = [
-    {titulo:"i origins",idade:18},
-    {titulo:"submarine",idade:14},
-    {titulo:"her",idade:16}
-]
+// let filmes = [
+//     {titulo:"i origins",idade:18},
+//     {titulo:"submarine",idade:14},
+//     {titulo:"her",idade:16}
+// ]
 
-let userIdade = prompt("digite sua idade: ")
+// let userIdade = prompt("digite sua idade: ")
 
-for(let i=0;i<filmes.length;i++){
-    if(userIdade>filmes[i].idade){
-        alert(`voce podera assistir ${filmes[i].titulo}`)
-    }else{
+// for(let i=0;i<filmes.length;i++){
+//     if(userIdade>filmes[i].idade){
+//         alert(`voce podera assistir ${filmes[i].titulo}`)
+//     }else{
      
-    }
+//     }
+// }
+
+
+// -------------------funçao calc--------------------------------------------
+
+function soma(a,b){
+    let soma = a+b
+    alert(`sua soma é ${soma}`)
 }
 
+function subt(a,b){
+    let subt = a-b
+    alert(`sua subtraçao é ${subt}`)
+}
 
- 
+function mult(a,b){
+    let mult = a*b
+    alert(`sua multiplicaçao é ${mult}`)
+}
+
+function div(a,b){
+    let div = a/b
+    alert(`sua soma é ${div}`)
+}
+
+let num1 = Number(prompt("digite o primeiro numero"))
+let num2 = Number(prompt("digite o segundo numero"))
+let calc = Number(prompt("digite 1 para efetuar a soma entre os valores, 2 para a subtraçao, 3 para a multiplicaçao e 4 para divisao"))
+
+if(calc == 1){
+    soma(num1, num2)
+}
+
+if(calc == 2){
+    subt(num1, num2)
+}
+
+if(calc == 3){
+    mult(num1, num2)
+}
+
+if(calc == 4){
+    div(num1, num2)
+}
