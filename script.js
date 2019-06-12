e43d8d7a1ba28a6e9270f7c4b6a5e6678b665741
“kaya <kaya.luanah@gmail.com>
2019-06-12 14:35:00 -0300

    utilizar string template recebido com prompt

--- script.js
+++ script.js
@@ -9,11 +9,21 @@
 // ---------------------------------------------------------------------------------
 
 
-let nome = 'kaya'
-let idade = '19'
-let signo = 'aquario'
+// let nome = 'kaya'
+// let idade = '19'
+// let signo = 'aquario'
 
-alert(`meu nome é ${nome}, tenho ${idade} anos e meu signo é de ${signo}!!`)
+// alert(`meu nome é ${nome}, tenho ${idade} anos e meu signo é de ${signo}!!`)
+
+
+// -----------------------------------------------------------------------------------
+
+
+let nome2 = prompt("Digite seu nome:")
+
+alert(`seu nome é ${nome2}`)
+
+
 
 
  
